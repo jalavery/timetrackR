ccb29751e398188d246745a369d941a739810839
Jessica Lavery <46568753+jalavery@users.noreply.github.com>
2020-02-25 07:15:49 -0500

    Add hex sticker, updates to ppt

---
+++ hexsticker.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker(subplot = "/Users/jessica/Desktop/calendar_light_orange.png", 
+        s_x = 1.05, s_y = .7, s_width = 0.5, s_height = 0.6,
+        h_fill = "#007CBA", h_color = "#006098",
+        package = "timetrackR", p_size = 8, p_color = "#F6C65B")
+        
